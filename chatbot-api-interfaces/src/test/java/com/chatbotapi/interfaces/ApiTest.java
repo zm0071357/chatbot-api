6688bfb1d9f62f84e759f80d337920dbd3e159f9
zm0071357 <834302225@qq.com>
2024-09-03 10:06:42 +0800

    进行单元测试

--- chatbot-api-interfaces/src/test/java/com/chatbotapi/interfaces/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/com/chatbotapi/interfaces/ApiTest.java
@@ -1,7 +1,66 @@
 package com.chatbotapi.interfaces;
 
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * 单元测试
  */
 public class ApiTest {
+
+    /**
+     * 获取提问
+     * @throws IOException
+     */
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("");
+        get.addHeader("cookie","");
+        get.addHeader("Content-Type","");
+        CloseableHttpResponse response = client.execute(get);
+
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    /**
+     * 回答提问
+     * @throws IOException
+     */
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost("");
+        post.addHeader("cookie","");
+        post.addHeader("Content-Type","");
+        String paramJson = "";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "utf-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = client.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
 }
